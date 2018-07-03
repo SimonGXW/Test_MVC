e0b3792633b893857ed6dd165e092c69bbc013bc
SimonGXW <840576829@qq.com>
2018-07-03 21:13:49 +0800

    添加项目文件。

---
+++ Test_MVC/Controllers/TestController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Test_MVC.Models;
+using Test_MVC.ViewModels;
+
+namespace Test_MVC.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+        public string GetString()
+        {
+            return "Hello World is old now. It’s time for wassup bro ;)";
+
+        }
+        public ActionResult GetView()
+        {
+            EmployeeListViewModel employeeListViewModel = new EmployeeListViewModel();
+
+            EmployeeBusinessLayer empBal = new EmployeeBusinessLayer();
+            List<Employee> employees = empBal.GetEmployees();
+
+            List<EmployeeViewModel> empViewModels = new List<EmployeeViewModel>();
+
+            foreach (Employee emp in employees)
+            {
+                EmployeeViewModel empViewModel = new EmployeeViewModel();
+                empViewModel.EmployeeName = emp.FirstName + " " + emp.LastName;
+                empViewModel.Salary = emp.Salary.ToString("C");
+                if (emp.Salary > 15000)
+                {
+                    empViewModel.SalaryColor = "yellow";
+                }
+                else
+                {
+                    empViewModel.SalaryColor = "green";
+                }
+                empViewModels.Add(empViewModel);
+            }
+            employeeListViewModel.Employees = empViewModels;
+            employeeListViewModel.UserName = "Admin";
+            return View("MyView", employeeListViewModel);
+        }
+
+
+
+
+        [NonAction]
+        public string SimpleMethod()
+        {
+            return "Hi, I am not action method";
+        }
+    }
+}
